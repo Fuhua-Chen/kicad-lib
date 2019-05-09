e588dec7ce933a81f0fd60f250a5fe450d0caa52
linlibing <978257694@qq.com>
2019-05-09 16:06:46 +0800

    增加红外接收管 Signed-off-by: linlibing <978257694@qq.com>

--- update from server.sh
+++
@@ -1,3 +0,0 @@
-echo "正在拉取服务器仓库数据"
-git fetch --all && git reset --hard origin/master && git pull
-read -p "按任意键退出" var
---
+++ update.sh
@@ -0,0 +1,5 @@
+﻿echo "正在拉取服务器仓库数据"
+git fetch origin master
+git merge origin/master
+git push
+read -p "按任意键退出" va
